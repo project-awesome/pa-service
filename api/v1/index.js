351e140677c0aaa100edf8ebadfee825fe7ac466
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-09-15 15:22:23 -0700

    Added moodle export functionality to api.

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,4 +1,6 @@
 var projectAwesome = require('project-awesome');
+var requestChecker = require('../../middleware/request-checker.js');
+var requireQuery = requestChecker.requireQuery;
 
 /*
 module.exports.isSeedValid = QuizValidator.isSeedValid;
@@ -12,28 +14,41 @@ module.exports.validateQuizDescriptor = QuizBuilder.validateQuizDescriptor;
 module.exports.buildQuiz = QuizBuilder.build;
 */
 
-function requireParam(param) {
-	return function(req, res, next) {
-		if (!(param in req.query)) {
-			res.status(400).end();
-			return;
-		}
-		next();
-	}
-}
-
 module.exports = function(app) {
 
-	app.get('/v1/is_seed_valid', requireParam('seed'), function(req, res) {
+	app.get('/v1/is_seed_valid', requireQuery('seed'), function(req, res) {
 		var is_seed_valid = projectAwesome.isSeedValid(req.query.seed);
 		res.json({ is_seed_valid: is_seed_valid }).end();
 	});
 
-	app.get('/v1/is_valid_question_type', requireParam('question_type'), function(req, res) {
+	app.get('/v1/is_valid_question_type', requireQuery('question_type'), function(req, res) {
 		var is_valid_question_type = projectAwesome.isValidQuestionType(req.query.question_type);
 		res.json({ is_valid_question_type: is_valid_question_type }).end();
 	});
 
+	app.get('/v1/generate_moodle_xml', 
+		requireQuery('question_type'), 
+		requireQuery('count'), 
+		requireQuery('question_name'), 
+		requireQuery('seed'), 
+		function(req, res) {
+			if (!projectAwesome.isSeedValid(req.query.seed)) {
+				res.status(400).end();
+				return;
+			}
+			if (!projectAwesome.isValidQuestionType(req.query.question_type)) {
+				res.status(400).end();
+				return;
+			}
+            var count = parseInt(req.query.count);
+            if (isNaN(count) || count < 1 || count > 1000) {
+                res.status(400).end();
+                return;
+            }
+        	res.set('Content-Type', 'text/xml');
+        	res.send(projectAwesome.generateMoodleXML(req.query.question_type, count, req.query.question_name, req.query.seed));
+		}
+	);
 }
 
 
--- test/api/v1/index.js
+++ test/api/v1/index.js
@@ -30,6 +30,145 @@ describe('v1 API', function() {
         server.close();
     });
 
+
+
+    describe('GET /generate_moodle_xml', function() {
+        describe('bad requests', function() {
+            describe('no question_type parameter', function() {
+                it('should respond with 400', function(done) {
+                    request(app)
+                    .get('/v1/generate_moodle_xml?count=10&question_name=blah&seed=blah')
+                    .expect(400)
+                    .end(done);
+                });
+            });
+            describe('no count parameter', function() {
+                it('should respond with 400', function(done) {
+                    request(app)
+                    .get('/v1/generate_moodle_xml?question_type=blah&question_name=blah&seed=blah')
+                    .expect(400)
+                    .end(done);
+                });
+            });
+            describe('no question_name parameter', function() {
+                it('should respond with 400', function(done) {
+                    request(app)
+                    .get('/v1/generate_moodle_xml?question_type=blah&count=10&seed=blah')
+                    .expect(400)
+                    .end(done);
+                });
+            });
+            describe('no seed parameter', function() {
+                it('should respond with 400', function(done) {
+                    request(app)
+                    .get('/v1/generate_moodle_xml?question_type=blah&count=10&question_name=blah')
+                    .expect(400)
+                    .end(done);
+                });
+            });
+        });
+        describe('when seed is invalid', function() {
+            var isSeedValidStub;
+            var isValidQuestionTypeStub;
+            before(function() {
+                isSeedValidStub = sinon.stub(projectAwesome, 'isSeedValid', function() { return false; });
+                isValidQuestionTypeStub = sinon.stub(projectAwesome, 'isValidQuestionType', function() { return true; });
+            });
+            after(function() {
+                isSeedValidStub.restore();
+                isValidQuestionTypeStub.restore();
+            });
+            it('should respond with 400', function(done) {
+                request(app)
+                .get('/v1/generate_moodle_xml?question_type=blah&count=10&question_name=blah&seed=blah')
+                .expect(400)
+                .end(done);
+            });
+        });
+        describe('when question type is invalid', function() {
+            var isSeedValidStub;
+            var isValidQuestionTypeStub;
+            before(function() {
+                isSeedValidStub = sinon.stub(projectAwesome, 'isSeedValid', function() { return true; });
+                isValidQuestionTypeStub = sinon.stub(projectAwesome, 'isValidQuestionType', function() { return false; });
+            });
+            after(function() {
+                isSeedValidStub.restore();
+                isValidQuestionTypeStub.restore();
+            });
+            it('should respond with 400', function(done) {
+                request(app)
+                .get('/v1/generate_moodle_xml?question_type=blah&count=10&question_name=blah&seed=blah')
+                .expect(400)
+                .end(done);
+            });
+        });
+        describe('when count is invalid', function() {
+            var isSeedValidStub;
+            var isValidQuestionTypeStub;
+            before(function() {
+                isSeedValidStub = sinon.stub(projectAwesome, 'isSeedValid', function() { return false; });
+                isValidQuestionTypeStub = sinon.stub(projectAwesome, 'isValidQuestionType', function() { return true; });
+            });
+            after(function() {
+                isSeedValidStub.restore();
+                isValidQuestionTypeStub.restore();
+            });
+            describe('when not an integer', function() {
+                it('should respond with 400', function(done) {
+                    request(app)
+                    .get('/v1/generate_moodle_xml?question_type=blah&count=notanint&question_name=blah&seed=blah')
+                    .expect(400)
+                    .end(done);
+                });
+            });
+            describe('when < 1', function() {
+                it('should respond with 400', function(done) {
+                    request(app)
+                    .get('/v1/generate_moodle_xml?question_type=blah&count=0&question_name=blah&seed=blah')
+                    .expect(400)
+                    .end(done);
+                });
+            });
+            describe('when > 1000', function() {
+                it('should respond with 400', function(done) {
+                    request(app)
+                    .get('/v1/generate_moodle_xml?question_type=blah&count=1001&question_name=blah&seed=blah')
+                    .expect(400)
+                    .end(done);
+                });
+            });
+        });
+        describe('valid parameters', function() {
+            var isSeedValidStub;
+            var isValidQuestionTypeStub;
+            var generateMoodleXMLStub;
+            before(function() {
+                isSeedValidStub = sinon.stub(projectAwesome, 'isSeedValid', function() { return true; });
+                isValidQuestionTypeStub = sinon.stub(projectAwesome, 'isValidQuestionType', function() { return true; });
+                generateMoodleXMLStub = sinon.stub(projectAwesome, 'generateMoodleXML', function() { return 'correctresult'; });
+            });
+            after(function() {
+                isSeedValidStub.restore();
+                isValidQuestionTypeStub.restore();
+                generateMoodleXMLStub.restore();
+            });
+            it('should call projectAwesome.generateMoodleXML', function(done) {
+                request(app)
+                .get('/v1/generate_moodle_xml?question_type=blah&count=1000&question_name=blah&seed=blah')
+                .expect('Content-Type', /xml/)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) return done(err);
+                    expect(generateMoodleXMLStub.calledOnce).to.be.true;
+                    expect(generateMoodleXMLStub.calledWith('blah', 1000, 'blah', 'blah')).to.be.true;
+                    expect(res.text).to.equal('correctresult');
+                    done();
+                });
+            });
+        });
+    });
+
     describe('GET /is_seed_valid', function() {
         describe('bad requests', function() {
             describe('no seed parameter', function() {
